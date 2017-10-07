de3909b0b34ed6ce64572d576d1c5bf39b30461e
Guilherme Varela <guilhermevarela@hotmail.com>
2017-10-06 21:27:03 -0300

    tse_sigla: added

--- spider_tse_siglas.py
+++ spider_tse_siglas.py
@@ -5,16 +5,38 @@
 	Author: Guilherme Varela
 
 	Scrapy shell: scrapy shell 'http://www.tse.jus.br/partidos/partidos-politicos/registrados-no-tse'
+
+	Scrapy running: scrapy runspider spider_tse_siglas.py
+
+	Scrapy run + store: scrapy runspider  spider_tse_siglas.py -o siglas.json
 '''
 import scrapy
 
 class TseSiglasSpider(scrapy.Spider):
 	name= 'tsesiglas'
 	start_urls = ['http://www.tse.jus.br/partidos/partidos-politicos/registrados-no-tse']
-
+	
 	def parse(self, response): 
 		first= True
-		headers= {} 
-		for title in response.css('table.grid.listing tbody'):			
-			yield {'title': title.css('::text').extract_first()}
+		trs = response.xpath('//tbody/tr')
+		ths = trs.xpath('./th')
+		headers = [] 
+		for th in ths:
+			h = th.xpath('.//text()').extract_first()
+			headers.append(h)
+			
+		yield {'headers': headers}
+		
+
+		tds = trs.xpath('.//td')
+		sigla= [] 
+		for i, td in enumerate(tds):			
+			n = i % len(headers)
+			if i>0 and n == 0:
+				yield {k:v for k,v in zip(headers, sigla)}				
+				sigla= [] 
+			
+			value = td.xpath('.//text()').extract_first()
+			sigla.append(value)			
+		yield {k:v for k,v in zip(headers, sigla)}				
 
