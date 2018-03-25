8cd3c9ab27f9525dca7ff620483266147360aeb0
Guilherme Varela <guilhermevarela@hotmail.com>
2018-03-25 17:46:18 -0300

    linking: datasets

--- transform.py
+++ transform.py
@@ -12,12 +12,28 @@ import re
 import pandas as pd
 import json
 
-def transform_file():
-    #upload person
 
-    #upload parties
-    parties_d={}
-    with open('datasets/raw/partidos_apicamarav2.json', mode='r') as f:
+def get_parties():
+    '''
+        Gets all parties related fields merging data from
+            parties_apicamarav2.json
+            internally generated uris slp
+            tse
+
+        returns:
+            df containing the parties
+                all columns are <providers>:<field_name>
+    '''
+    # upload slp parties
+    df = pd.read_csv('resource_uri/party_resource_uri.csv', sep= ';', index_col= None)
+    df = df.rename(columns={'Nome':'name', 'party_resource_uri':'resource_uri'})
+    prov = ['slp']*len(df.columns)
+    # import code; code.interact(local=dict(globals(), **locals()))
+    new_index = pd.MultiIndex.from_tuples(zip(prov, df.columns.names))
+
+    # upload camara api parties
+    parties_d = {}
+    with open('datasets/raw/parties_apicamarav2.json', mode='r') as f:
         parties = json.load(f)['dados']
     f.close()
 
@@ -27,15 +43,17 @@ def transform_file():
                 parties_d[key] = {}
             parties_d[key][i] = value
 
-    df_parties =  pd.DataFrame.from_dict(parties_d)
-    df_parties = df_parties.set_index('uri')
+    _df = pd.DataFrame.from_dict(parties_d)
+    _df = df_parties.set_index('uri')
 
-        
-    import code; code.interact(local=dict(globals(), **locals()))
-    # parties = json.loads('datasets/raw/partidos_apicamarav2.json')
+    df  = df.join(_df, on= 'sigla', how= 'left')
     
-    df_parties = df_parties.set_index('id')
-    import code; code.interact(local=dict(globals(), **locals()))
+    return df_parties
+
+
+def transform_file():
+    #upload person    
+    df_parties = get_parties()
 
     re_triple = re.compile(r'<(.*?)>')
     re_post   = re.compile(r'postIn')
@@ -50,8 +68,10 @@ def transform_file():
         _predicate = re_triple.match(txt[l:]).groups()[0]
         l += len(_predicate) + 3
         _object = re_triple.match(txt[l:]).groups()[0]
-        # if re_post(_predicate):
-        #subject is a foaf agent, object is party
+        
+        if re_post.search(_predicate):
+            # subject is a foaf agent, object is party            
+            print('foaf:agent')
         
 
 
